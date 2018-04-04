b0b683c9fc7515afef87047f96dbbca3e3118898
sooheelee <sooheelee@users.noreply.github.com>
2018-04-03 22:57:54 -0400

    Fix toast message to get strings of R ids. (#3)
    * Account for space trimming by getString method

--- MainActivity.java
+++ MainActivity.java
@@ -117,9 +117,9 @@ public class MainActivity extends AppCompatActivity {
         String[] q07_answers = {"5", "1", "2", "4", "3"};
         numberCorrect = numberCorrect + getRankingAnswer(q07, q07_answers);
 
-        String grade_message = numberCorrect + R.string.out_of + numberOfQuestions + R.string.correct2;
+        String grade_message = numberCorrect + " " + getString(R.string.out_of) + " " + numberOfQuestions + " " + getString(R.string.correct);
         if (numberCorrect == 7) {
-            grade_message = grade_message + R.string.excellent;
+            grade_message = grade_message + getString(R.string.excellent);
         }
         toast(grade_message);
     }
